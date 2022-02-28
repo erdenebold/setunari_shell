aa550473bd1a589603b287809559344f91087dd6
erka <erdenebold10@gmail.com>
2022-02-28 09:40:37 +0800

    code update hiij bn

---
+++ setunari_update.sh
@@ -0,0 +1,9 @@
+cd /opt/odoo/setunari_erp
+sudo git pull
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server start
+
